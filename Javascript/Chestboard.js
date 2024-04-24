8b88a2dca361d04cd90d2f0bfff5714454788515
Noelli117 <nollli0117@gmail.com>
2024-04-23 21:04:40 -0400

    1

---
+++ Javascript/Chestboard.js
@@ -0,0 +1,20 @@
+//Defining function for printing the chessboard.
+function printChessboard(size) {
+    let board = ''; //Initializing the variable.
+    for (let row = 0; row < size; row++) { //Same nested forloop thing we had used in the pyramid project.
+        for (let col = 0; col < size; col++) {
+            if ((row + col) % 2 === 0) {//Determining where to put # or space;add space when it's even, otherwise #.
+                board += ' ';
+            } else {
+                board += '#';
+            }
+            if (col === size - 1) {
+                board += '\n';
+            }
+        }
+    }
+    console.log(board);
+}
+
+const size = 8; // Change this value to change the size of the chessboard
+printChessboard(size);
