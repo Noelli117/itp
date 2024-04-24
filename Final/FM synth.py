f80d212a4bf0a49d866cf347833b700e1f26cd7c
Noelli117 <nollli0117@gmail.com>
2024-04-23 20:46:29 -0400

    1

--- Final/FM synth.py
+++ Final/FM synth.py
@@ -6,9 +6,9 @@ import sounddevice as sd
 #Parameters that controls the waveform:
 f_s = 44100
 duration_s = 3
-f_c= 200.0
-f_m= 11000.0
-k = 20
+f_c= 400.0
+f_m= 10000.0
+k = 10
 #f_s=sample rate,duration_s=duration time in seconds,f_c=carrier frequency, f_m=modulation frequency,k=modulator index
 
 #Generated time array
